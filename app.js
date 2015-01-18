2f0c61b22d90b96d58802a82c050ad386acfc514
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-01-18 09:17:46 -0500

    ??

--- app.js
+++ app.js
@@ -197,7 +197,7 @@ app.post('/api/glass', function(req, res){
 
   Request.open('POST', 'https://api.imgur.com/3/upload');
 
-  Request.setRequestHeader('Authorization', 'f948415a877272b');
+  Request.setRequestHeader('Authorization', 'Client-ID f948415a877272b');
 
 
   Request.onreadystatechange = function () {
